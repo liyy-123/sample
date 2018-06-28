f6190ddf5084f30e2771825aaf40b9b2e8c91c96
vagrant <vagrant@homestead>
2018-06-28 09:59:07 +0000

    dierci

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>@yield('title','Sample') - The Way of Laravel</title>
+  </head>
+  <body>
+    @yield('content')
+  </body>
+</html>
+
+<!-- git remote add origin git@github.com:liyy-123/sample.git
+vagrant@homestead:~/Code/sample$ git push origin master
+-->
